aa8e152e380705b37125d9e5e55d3e8e47a4d737
Vicente Vendramin <guesser.vicente@gmail.com>
2025-01-21 17:21:40 -0300

    feat: add header component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
